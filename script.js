5319e2e11658d3ee72ea2d5f0de24b59b06f09f0
David Craft <david@gallery263.org>
2023-10-19 10:48:17 -0400

    add javascript code

---
+++ script.js
@@ -0,0 +1,36 @@
+const imageFiles = ['day1_1.jpg', 'day1_2.jpg']; // Add more for each day
+const hintFiles = ['day1_1.txt', 'day1_2.txt']; // Add more for each day
+
+let currentDay = 0; // Start with day 1
+
+const celebrityImage = document.getElementById('celebrity-image');
+const hintText = document.getElementById('hint-text');
+const guessInput = document.getElementById('guess-input');
+const submitButton = document.getElementById('submit-button');
+const nextHintButton = document.getElementById('next-hint-button');
+
+function loadDay(day) {
+    // Set the image source and hint text based on the current day
+    celebrityImage.src = imageFiles[day];
+    fetch(hintFiles[day])
+        .then((response) => response.text())
+        .then((hint) => {
+            hintText.textContent = hint;
+        });
+}
+
+submitButton.addEventListener('click', () => {
+    const playerGuess = guessInput.value.trim();
+    // Add logic to check the guess and display feedback
+});
+
+nextHintButton.addEventListener('click', () => {
+    currentDay++;
+    if (currentDay < imageFiles.length) {
+        loadDay(currentDay);
+    } else {
+        alert('You have seen all hints for this day.');
+    }
+});
+
+loadDay(currentDay);
